84572e55e53ff622e9b5efd4ffe454c4ea8a17fe
Park9eon <quiet9.noise@gmail.com>
2019-01-22 13:56:23 +0900

    vm2 사용

--- test.js
+++ test.js
@@ -3,6 +3,8 @@
 /**
  * Created by park9eon on 2019-01-16
  */
+const {VM} = require('vm2');
+
 const obj = {
     string: "String",
     int: 1,
@@ -10,50 +12,58 @@ const obj = {
     date: new Date(),
     object: {
         child: "child"
-    }
+    },
+    list: [1, 2, 3],
 };
 
 const template = `
 {
-    "string": it.string,
-    "int": it.int,
-    "float": it.float,
-    "date": it.date,
-    "dateString": it.date.toString(),
-    "object": it.object,
-    "child": it.object && it.object.child,
-    "boolean": it.int === it.float,
-    "list": [it.int, it.float],
-    "eval": [eval('global'), module, Function, require, process],
+    "string": string,
+    "int": int,
+    "float": float,
+    "date": date,
+    "dateString": date.toString(),
+    "object": object,
+    "child": object && object.child,
+    "boolean": int === float,
+    "list": list,
+    "customList": [int, float],
     "self": {
-        "int": it.int,
+        "int": int,
     },
-    "math": Math.floor(it.float),
-    ["nameWithString" + it.int]: "nameWithString" + it.int
+    "math": Math.floor(float),
+    ["nameWString" + int]: "nameWString" + int,
+    "listWithMap": list.map(n => n + 1),
 };
 `;
 
-function render(object, template) {
-    function func(object) {
-        return new Function("it",
-            // language=JavaScript
-            `"use strict";
-            const process = undefined;
-            const require = undefined;
-            const Function = undefined;
-            const module = undefined;
-            const console = undefined;
-            const template = ${template};
-            return template;
-        `).bind(object)(object);
+function render(obj, template) {
+    function func(obj) {
+        return new VM({sandbox: obj}).run(`
+            const _ = ${template};_;
+        `)
     }
 
-    if (Array.isArray(object)) {
-        return object.map(func)
+    if (Array.isArray(obj)) {
+        return obj.map((item) => func(item));
     } else {
-        return func(object);
+        return func(obj)
     }
 }
 
-const result = render([obj], template);
-console.log(result);
+const cpuUsage = process.cpuUsage();
+
+for (let i = 0; i < 10000; i++) {
+    console.log(render([obj], template));
+    console.log(`MEM : ${process.memoryUsage().heapTotal * 1e-6}MB`);
+}
+
+// VM 사용
+// MEM : 96.71475199999999MB
+// { user: 9447318, system: 934638 }
+
+// 단순 포문
+// MEM : 17.022976MB
+// { user: 121796, system: 27970 }
+
+console.log(process.cpuUsage(cpuUsage));
